807f8381059764bdb5d20ddeab12e51d92b9214b
Nícolas Iensen <nicolas@engage.is>
2013-05-14 12:14:41 -0300

    transactions#index style

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,4 +8,9 @@ class Transaction < ActiveRecord::Base
   scope :outgoing, where("value < 0 AND description != ?", "BB Renda Fixa 200")
   scope :this_month, where("date >= ?", Date.today.at_beginning_of_month)
   before_create { self.key = Digest::MD5.hexdigest(self.date.to_s + self.description + self.document_number + self.value.to_s) }
+
+  def self.by_month month, year
+    date = Date.parse("1/#{month}/#{year}")
+    where("date >= ? AND date <= ?", date.at_beginning_of_month, date.at_end_of_month)
+  end
 end
